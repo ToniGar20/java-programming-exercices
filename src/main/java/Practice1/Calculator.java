12e337bbc8df41038139271375290a3205aa71a6
Toni <garcia.t20@gmail.com>
2021-04-19 11:48:18 +0200

    2 fixes

--- src/main/java/Practice1/Calculator.java
+++ src/main/java/Practice1/Calculator.java
@@ -5,22 +5,22 @@ import java.util.Scanner;
 public class Calculator {
 
     static void division (double a, double b){
-        System.out.println("---------------------------");
+        System.out.println("--------------------------------------");
         System.out.println("El resultado de la DIVISIÓN es " + (a/b));
     }
 
     static void multiplication (double a, double b){
-        System.out.println("---------------------------");
+        System.out.println("--------------------------------------");
         System.out.println("El resultado de la MULTIPLICACIÓN es " + (a*b));
     }
 
     static void substraction (double a, double b){
-        System.out.println("---------------------------");
+        System.out.println("--------------------------------------");
         System.out.println("El resultado de la RESTA es " + (a-b));
     }
 
     static void addition (double a, double b){
-        System.out.println("---------------------------");
+        System.out.println("--------------------------------------");
         System.out.println("El resultado de la SUMA es " + (a+b));
     }
 
@@ -38,38 +38,48 @@ public class Calculator {
         return b;
     }
 
+    public static  boolean repeat = true;
+
     public static void main(String[] args) {
-        System.out.println("MENÚ PRINCIPAL DE LA CALCULADORA");
-        System.out.println("---------------------------");
-        System.out.println("Introduce una de las siguientes opciones ");
-        System.out.println("1. Sumar");
-        System.out.println("2. Restar");
-        System.out.println("3. Multiplicar");
-        System.out.println("4. Dividir");
-        System.out.println("---------------------------");
-        System.out.println("0. Finalizar la aplicación");
-        System.out.println("---------------------------");
+        System.out.println("BIENVENIDO A LA APLICACIÓN CALCULADORA");
 
-        Scanner operationSelected = new Scanner(System.in);
-        System.out.print("Operación seleccionada ");
-        int operation = operationSelected.nextInt();
+        while (repeat) {
+            System.out.println("--------------------------------------");
+            System.out.println("MENÚ PRINCIPAL");
+            System.out.println("Introduce una de las siguientes opciones para realizar una operación ");
+            System.out.println("1. Sumar");
+            System.out.println("2. Restar");
+            System.out.println("3. Multiplicar");
+            System.out.println("4. Dividir");
+            System.out.println("--------------------------------------");
+            System.out.println("0. Finalizar la aplicación");
+            System.out.println("--------------------------------------");
 
-        switch (operation) {
-            case 1:
-                addition(getFirstValue(), getSecondValue());
-            case 2:
-                substraction(getFirstValue(), getSecondValue());
-            case 3:
-                multiplication(getFirstValue(), getSecondValue());
-            case 4:
-                division(getFirstValue(), getSecondValue());
-            case 0:
-                System.out.println("La aplicación ha finalizado.");
-                break;
-            default:
-                System.out.println("El valor introducido es incorrecto, prueba de nuevo.");
+            Scanner operationSelected = new Scanner(System.in);
+            System.out.print("Operación seleccionada ");
+            int operation = operationSelected.nextInt();
+
+            switch (operation) {
+                case 1:
+                    addition(getFirstValue(), getSecondValue());
+                    break;
+                case 2:
+                    substraction(getFirstValue(), getSecondValue());
+                    break;
+                case 3:
+                    multiplication(getFirstValue(), getSecondValue());
+                    break;
+                case 4:
+                    division(getFirstValue(), getSecondValue());
+                    break;
+                case 0:
+                    System.out.println("La aplicación ha finalizado.");
+                    repeat=false;
+                    break;
+                default:
+                    System.out.println("El valor introducido es incorrecto, prueba de nuevo.");
+            }
         }
-
 
     }
 }
